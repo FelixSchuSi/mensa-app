3869a8709b10cf4e66cb76a00eb13ef235c0bb20
Norman Lahme-Hütig <nl197854@fh-muenster.de>
2020-10-19 09:12:13 +0200

    k2.6-u1-m1

--- client/src/components/sign-in/sign-in.component.ts
+++ client/src/components/sign-in/sign-in.component.ts
@@ -8,6 +8,15 @@ class SignInComponent extends LitElement {
     ${unsafeCSS(componentCSS)}
   `;
 
+  @query('form')
+  form!: HTMLFormElement;
+
+  @query('#email')
+  emailElement!: HTMLInputElement;
+
+  @query('#password')
+  passwordElement!: HTMLInputElement;
+
   render() {
     return html`
       <h1>Anmelden</h1>
@@ -15,15 +24,31 @@ class SignInComponent extends LitElement {
         <div>
           <label for="email">E-Mail</label>
           <input type="email" autofocus required id="email" name="email" />
+          <div class="invalid-feedback">E-Mail ist erforderlich und muss gültig sein</div>
         </div>
         <div>
           <label for="password">Passwort</label>
           <input type="password" required id="password" name="password" />
+          <div class="invalid-feedback">Passwort ist erforderlich</div>
         </div>
         <button type="button" @click="${this.submit}">Anmelden</button>
       </form>
     `;
   }
 
-  async submit(event: Event) {}
+  async submit() {
+    if (this.isFormValid()) {
+      const authData = {
+        email: this.emailElement.value,
+        password: this.passwordElement.value
+      };
+      // ...
+    } else {
+      this.form.classList.add('was-validated');
+    }
+  }
+
+  isFormValid() {
+    return this.form.checkValidity();
+  }
 }
