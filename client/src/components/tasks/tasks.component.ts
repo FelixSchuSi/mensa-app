901c4fb05a5b5b74a60be0bc373a7be4ffc4bf90
Norman Lahme-Hütig <nl197854@fh-muenster.de>
2020-10-19 09:12:04 +0200

    k2.4-u1-a1

--- client/src/components/tasks/tasks.component.ts
+++ client/src/components/tasks/tasks.component.ts
@@ -1,4 +1,4 @@
-import { html, render } from 'lit-html';
+import { css, customElement, html, LitElement, property, unsafeCSS } from 'lit-element';
 import { repeat } from 'lit-html/directives/repeat';
 import { guard } from 'lit-html/directives/guard';
 
@@ -10,39 +10,19 @@ interface Task {
 
 const componentCSS = require('./tasks.component.scss');
 
-class TasksComponent extends HTMLElement {
-  private renderTriggered = false;
+@customElement('app-tasks')
+class TasksComponent extends LitElement {
+  static styles = css`
+    ${unsafeCSS(componentCSS)}
+  `;
 
-  private tasks: Task[] = [
+  @property() private tasks: Task[] = [
     { id: '0', title: 'TypeScript lernen', status: 'done' },
     { id: '1', title: 'Web Components lernen', status: 'open' }
   ];
 
-  constructor() {
-    super();
-    this.attachShadow({ mode: 'open' });
-    this.invalidate();
-  }
-
   render() {
-    render(this.tasksTemplate(), this.shadowRoot!);
-  }
-
-  invalidate() {
-    if (!this.renderTriggered) {
-      this.renderTriggered = true;
-      Promise.resolve().then(() => {
-        this.renderTriggered = false;
-        this.render();
-      });
-    }
-  }
-
-  tasksTemplate() {
     return html`
-      <style>
-        ${componentCSS}
-      </style>
       <h1>Aufgaben</h1>
       <div class="tasks">
         ${guard(
@@ -71,13 +51,9 @@ class TasksComponent extends HTMLElement {
     this.tasks = this.tasks.map(task =>
       task === taskToToggle ? ({ ...task, status: (task.status || 'open') === 'open' ? 'done' : 'open' } as Task) : task
     );
-    this.invalidate();
   }
 
   removeTask(taskToRemove: Task) {
     this.tasks = this.tasks.filter(task => task.id !== taskToRemove.id);
-    this.invalidate();
   }
 }
-
-customElements.define('app-tasks', TasksComponent);
--- client/src/components/widgets/task/task.component.ts
+++ client/src/components/widgets/task/task.component.ts
@@ -1,45 +1,19 @@
-import { html, render } from 'lit-html';
+import { css, customElement, html, LitElement, property, unsafeCSS } from 'lit-element';
 
 const componentCSS = require('./task.component.scss');
 
-class TaskComponent extends HTMLElement {
-  private renderTriggered = false;
+@customElement('app-task')
+class TaskComponent extends LitElement {
+  static styles = css`
+    ${unsafeCSS(componentCSS)}
+  `;
 
-  constructor() {
-    super();
-    this.attachShadow({ mode: 'open' });
-  }
-
-  static get observedAttributes() {
-    return ['status'];
-  }
-
-  attributeChangedCallback(name: string, oldValue: string, newValue: string) {
-    this.invalidate();
-  }
+  @property() status = 'open';
 
   render() {
-    const status = this.getAttribute('status') || 'open';
-    render(this.taskTemplate(status), this.shadowRoot!);
-  }
-
-  invalidate() {
-    if (!this.renderTriggered) {
-      this.renderTriggered = true;
-      Promise.resolve().then(() => {
-        this.renderTriggered = false;
-        this.render();
-      });
-    }
-  }
-
-  taskTemplate(status: string) {
     return html`
-      <style>
-        ${componentCSS}
-      </style>
       <span class="toggle-task" @click="${() => this.emit('apptaskstatusclick')}"
-        >${status === 'done' ? html`<span class="status"></span>` : ''}</span
+        >${this.status === 'done' ? html`<span class="status"></span>` : ''}</span
       >
       <slot name="title"></slot>
       <span class="remove-task" @click="${() => this.emit('apptaskremoveclick')}"></span>
@@ -57,5 +31,3 @@ class TaskComponent extends HTMLElement {
     this.dispatchEvent(event);
   }
 }
-
-customElements.define('app-task', TaskComponent);
