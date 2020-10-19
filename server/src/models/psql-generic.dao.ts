7fc2d327a58223c4bf35e49f4d4816e7e686d6f9
Norman Lahme-Hütig <nl197854@fh-muenster.de>
2020-10-19 09:13:29 +0200

    k6.2-u1-a3

--- server/src/models/psql-generic.dao.ts
+++ server/src/models/psql-generic.dao.ts
@@ -56,7 +56,7 @@ function toColumnName(propertyName: string) {
 
 function toColumnValue(value: unknown) {
   if (typeof value === 'string') {
-    return `'` + value + `'`;
+    return `'` + value.replace(/'/g, `''`) + `'`;
   } else {
     return value;
   }
