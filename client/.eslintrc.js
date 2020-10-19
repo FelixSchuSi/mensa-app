1e6dbf16cd5eaa0baf85758d152f0815c188bde8
Norman Lahme-Hütig <nl197854@fh-muenster.de>
2020-10-19 09:12:46 +0200

    k3.2-u2-m1

--- client/.eslintrc.js
+++ client/.eslintrc.js
@@ -4,5 +4,11 @@ module.exports = {
   parser: '@typescript-eslint/parser',
   parserOptions: { ecmaVersion: 2015 },
   extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'plugin:prettier/recommended'],
-  rules: {}
+  rules: {
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/member-ordering': 'error',
+    '@typescript-eslint/no-non-null-assertion': 'off',
+    '@typescript-eslint/no-use-before-define': ['error', { functions: false }],
+    '@typescript-eslint/no-var-requires': 'off'
+  }
 };
