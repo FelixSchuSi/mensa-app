9819ff395b82cfc9dbac310eabf9cdf81eed7e2b
Norman Lahme-Hütig <nl197854@fh-muenster.de>
2020-10-19 09:11:37 +0200

    k2.2-u3-a1

--- client/src/components/task/task.component.ts
+++ client/src/components/task/task.component.ts
@@ -1,10 +1,59 @@
+const taskTemplate = document.createElement('template');
+taskTemplate.innerHTML = `
+  <style>
+    :host {
+      display: flex;
+      align-items: center;
+      padding: 1rem;
+      font-size: 1rem;
+      border: solid rgba(0, 0, 0, 0.125) 1px;
+    }
+    :host(:first-of-type) {
+      border-top-right-radius: .25rem;
+      border-top-left-radius: .25rem;
+    }
+
+    :host(:not(:first-of-type)) {
+      border-top: none;
+    }
+
+    :host(:last-of-type) {
+      border-bottom-right-radius: .25rem;
+      border-bottom-left-radius: .25rem;
+    }
+
+    .toggle-task {
+      display: inline-flex;
+      flex-shrink: 0;
+      width: 1.75rem;
+      height: 1.75rem;
+      border: 1px solid #888;
+      border-radius: 50%;
+      justify-content: center;
+      align-items: center;
+    }
+    
+    slot[name='title']::slotted(*) { flex-grow:1; margin-left: 0.75rem }
+    
+    .status::before {
+      color: #008000;
+      font: normal normal normal 1.25rem/1 'Font Awesome 5 Free';
+      content: "\\f00c"
+    }
+  </style>
+  <span class="toggle-task"><span class="status"></span></span>
+  <slot name="title"></slot>
+  `;
+
 class TaskComponent extends HTMLElement {
   constructor() {
     super();
+    this.attachShadow({ mode: 'open' });
+    this.shadowRoot!.appendChild(taskTemplate.content.cloneNode(true));
   }
 
   static get observedAttributes() {
-    return ['status', 'title'];
+    return ['status'];
   }
 
   attributeChangedCallback(name: string, oldValue: string, newValue: string) {
@@ -12,53 +61,8 @@ class TaskComponent extends HTMLElement {
   }
 
   render() {
-    const status = this.getAttribute('status');
-    const title = this.getAttribute('title');
-    const style = `
-      .task {
-        display: flex;
-        align-items: center;
-        padding: 1rem;
-        font-size: 1rem;
-        border: solid rgba(0, 0, 0, 0.125) 1px;
-      }
-      .task:first-of-type {
-        border-top-right-radius: .25rem;
-        border-top-left-radius: .25rem;
-      }
-  
-      .task:not(:first-of-type) {
-        border-top: none;
-      }
-  
-      .task:last-of-type {
-        border-bottom-right-radius: .25rem;
-        border-bottom-left-radius: .25rem;
-      }            
-      .toggle-task {
-        display: inline-flex;
-        flex-shrink: 0;
-        width: 1.75rem;
-        height: 1.75rem;
-        border: 1px solid #888;
-        border-radius: 50%;
-        justify-content: center;
-        align-items: center;
-      }
-      .title { flex-grow:1; margin-left: 0.75rem;}
-      .status::before {
-        color: #008000;
-        font: normal normal normal 1.25rem/1 'Font Awesome 5 Free';
-        content: "\\f00c"
-      }
-      `;
-    this.innerHTML = `
-        <style>${style}</style>
-        <div class="task">
-          <span class="toggle-task">${status === 'done' ? `<span class="status"></span>` : ''}</span>
-          <span class="title">${title}</span>
-        </div>
-      `;
+    const statusElem: any = this.shadowRoot!.querySelector('.status')!;
+    statusElem.style.display = this.getAttribute('status') === 'open' ? 'none' : 'inherit';
   }
 }
 
