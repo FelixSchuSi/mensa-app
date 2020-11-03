aad3a86c0b52a8ac8d1721fee428d0b4177707c6
Felix Schulze Sindern <felixs@fh-muenster.de>
2020-11-03 20:58:29 +0100

    Change backend address to production.

--- client/src/http-client.ts
+++ client/src/http-client.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@fhms-wi/http-client';
 
 // local development
-// export const httpClient = new HttpClient({ baseURL: 'https:' + '//' + location.hostname + ':3443/api/' });
+// export const httpClient = new HttpClient({ baseURL: 'http:' + '//' + location.hostname + ':3443/api/' });
 
 // production
-export const httpClient = new HttpClient({ baseURL: 'https://mensa-app-5jrmv.ondigitalocean.app:3443/api/' });
+export const httpClient = new HttpClient({ baseURL: 'https://mensa-app-5jrmv.ondigitalocean.app/api/' });
