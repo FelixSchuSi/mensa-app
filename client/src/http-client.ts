07c264be8053ca56d20734f8a6a302401cabe6bf
Felix Schulze Sindern <felixs@fh-muenster.de>
2020-11-04 22:55:23 +0100

    Correct use of build constant and improve styles.

--- client/src/http-client.ts
+++ client/src/http-client.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@fhms-wi/http-client';
 
-// ts-ignore
+// @ts-ignore
 const baseURL: string = ISPROD ? 'https://mensa-app-5jrmv.ondigitalocean.app/api/' : `http://${location.hostname}:3443/api/`;
 
 export const httpClient = new HttpClient({ baseURL });
