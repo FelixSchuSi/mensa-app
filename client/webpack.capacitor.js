2cf9724755771a536567889cb1262b12b2ff895d
Felix Schulze Sindern <felixs@fh-muenster.de>
2020-11-14 17:26:09 +0100

    Add path import to capacitor config.

--- client/webpack.capacitor.js
+++ client/webpack.capacitor.js
@@ -3,6 +3,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const { DefinePlugin } = require('webpack');
 const CopyPlugin = require('copy-webpack-plugin');
+const path = require('path');
 
 module.exports = {
   mode: 'production',
