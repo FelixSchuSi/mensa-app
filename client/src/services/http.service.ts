09ea9879fd9e0337c791341a5d35231550c4f2cc
Felix Schulze Sindern <felixs@fh-muenster.de>
2020-11-25 22:35:57 +0100

    Implement networkFirst strategy.

--- client/src/services/http.service.ts
+++ client/src/services/http.service.ts
@@ -3,9 +3,14 @@ interface HttpServiceConfig {
 }
 
 class HttpService {
+  private cache!: Cache;
   constructor(private config: HttpServiceConfig) {}
 
-  public get(url: string): Promise<Response> {
+  private async initCache(): Promise<void> {
+    this.cache = await caches.open('offline-cache');
+  }
+
+  public async get(url: string): Promise<Response> {
     return this.createFetch('GET', url);
   }
 
@@ -26,15 +31,12 @@ class HttpService {
   }
 
   private async createFetch(method: string, url: string, body?: unknown): Promise<Response> {
-    const requestInit: RequestInit = {
-      headers: { 'Content-Type': 'application/json; charset=utf-8' },
-      method: method,
-      credentials: 'include'
-    };
-    if (body) {
-      requestInit.body = JSON.stringify(body);
-    }
-    const response: Response = await fetch(this.config.baseURL + url, requestInit);
+    if (!this.cache) await this.initCache();
+
+    const request: Request = this.buildRequest(method, url, body);
+
+    const response: Response = await this.networkFirst(request);
+
     if (response.ok) {
       return response;
     } else {
@@ -47,8 +49,41 @@ class HttpService {
       return Promise.reject({ message, statusCode: response.status });
     }
   }
+
+  private buildRequest(method: string, url: string, body?: unknown): Request {
+    const requestInit: RequestInit = {
+      headers: { 'Content-Type': 'application/json; charset=utf-8' },
+      method: method,
+      credentials: 'include'
+    };
+
+    if (body) {
+      requestInit.body = JSON.stringify(body);
+    }
+    return new Request(this.config.baseURL + url, requestInit);
+  }
+
+  private async networkFirst(request: Request): Promise<Response> {
+    const NO_INTERNET = { message: 'Es konnte keine Verbindung hergestellt werden', statusCode: 503 };
+    if (navigator.onLine) {
+      const response = await fetch(request);
+      if (request.method === 'GET' && response.ok) await this.cache.put(request, response.clone());
+      return response;
+    } else {
+      try {
+        console.log('returning from network cache');
+        const cacheResult = await this.cache.match(request);
+        if (cacheResult === undefined) return Promise.reject(NO_INTERNET);
+        return cacheResult;
+      } catch (e) {
+        console.log(e);
+        return Promise.reject(NO_INTERNET);
+      }
+    }
+  }
 }
 
+// const ISPROD = true;
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-ignore
 const baseURL: string = ISPROD
