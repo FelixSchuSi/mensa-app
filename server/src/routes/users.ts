28f8663cf9703c54db4555f0be6a1820f5ebecfb
Felix Schulze Sindern <felixs@fh-muenster.de>
2020-12-12 22:41:27 +0100

    test new cookie settings

--- server/src/routes/users.ts
+++ server/src/routes/users.ts
@@ -7,7 +7,7 @@ import { User } from '../models/user';
 const router = express.Router();
 const isProd: boolean = !!process.env.ISPROD;
 const cookieOptions: CookieOptions = isProd
-  ? { sameSite: 'none', secure: true, httpOnly: false, domain: 'felixschusi.github.io' }
+  ? { sameSite: 'none', secure: true, httpOnly: false, domain: 'github.io' }
   : { sameSite: 'lax', httpOnly: false };
 
 router.post('/', async (req, res) => {
