492da72ed74e4f1b2f96c33cca3329dd18784857
Felix Schulze Sindern <felixs@fh-muenster.de>
2020-12-13 17:57:53 +0100

    Try new type of versioning in sw.

--- client/service-worker.js
+++ client/service-worker.js
@@ -1,17 +1,15 @@
 import { precacheAndRoute } from 'workbox-precaching/precacheAndRoute';
-const version = '1';
+const newVersion = '1';
+const oldVersion = localStorage.getItem('_sw_version');
+if (Number(newVersion) !== Number(oldVersion)) {
+  console.log('New SW Version found - clearing network cache.');
+  caches.keys().then(cacheNames => {
+    cacheNames.forEach(cacheName => {
+      caches.delete(cacheName);
+    });
+  });
+  localStorage.setItem('_sw_version', newVersion);
+}
 
-precacheAndRoute({ ...self.__WB_MANIFEST, revision: version });
-console.log('⚙️ Service Worker attached! ', self.__WB_MANIFEST);
-
-// then.addEventListener('activate', function (event) {
-//   var cacheWhitelist = ['v2'];
-
-//   event.waitUntil(
-//     caches.forEach(function (cache, cacheName) {
-//       if (cacheWhitelist.indexOf(cacheName) == -1) {
-//         return caches.delete(cacheName);
-//       }
-//     })
-//   );
-// });
+precacheAndRoute(self.__WB_MANIFEST);
+console.log('⚙️ Service Worker attached! ');
