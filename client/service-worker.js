8fb7d6a67517401eec3b583a58998055821d033c
Felix Schulze Sindern <felixs@fh-muenster.de>
2020-12-13 16:41:18 +0100

    Try different version number.

--- client/service-worker.js
+++ client/service-worker.js
@@ -1,5 +1,5 @@
 import { precacheAndRoute } from 'workbox-precaching/precacheAndRoute';
-const version = 1;
+const version = '1';
 
 precacheAndRoute({ ...self.__WB_MANIFEST, revision: version });
 console.log('⚙️ Service Worker attached! ', self.__WB_MANIFEST);
