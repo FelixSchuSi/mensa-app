3ed4fdc85e7bfd574d01bc9ea051dc5a09ba3ef8
Felix Schulze Sindern <felixs@fh-muenster.de>
2020-12-17 16:02:14 +0100

    progress

--- client/src/pages/meals-future/meals-future.page.ts
+++ client/src/pages/meals-future/meals-future.page.ts
@@ -2,6 +2,7 @@ import { css, customElement, html, LitElement, property, TemplateResult, unsafeC
 import { PageMixin } from '../page.mixin';
 import { LanguageStrings } from '../../models/language-strings';
 import { httpService } from '../../services/http.service';
+import { Meal } from '../../../../server/src/models/meal';
 
 const sharedCSS = require('../../shared.scss');
 const componentCSS = require('./meals-future.page.scss');
@@ -21,15 +22,25 @@ class MealsFuturePage extends PageMixin(LitElement) {
   @property({ type: Object, attribute: false })
   protected i18n!: LanguageStrings;
 
+  @property({ attribute: false })
+  protected meals: Meal[] = [];
+
   protected async firstUpdated(): Promise<void> {
     try {
       const res = await httpService.get('meals');
       const json = await res.json();
+      this.meals = json.results;
       console.log(json);
     } catch (e) {}
   }
 
   protected render(): TemplateResult {
-    return html` ${this.renderNotification()} Hier könnten Ihre Gerichte stehen! `;
+    return html` ${this.renderNotification()} Hier könnten Ihre Gerichte stehen!
+    ${this.meals.map(meal => {
+      const { title, date, mensa, additives, allergens, otherInfo, price } = meal;
+      const entries = Object.entries({ date, mensa, additives, allergens, otherInfo, price });
+      return html` <h2>${title}</h2>
+        ${entries.map(([key, value]) => html`<span><b>${key}</b>${value}</span><br />`)} <br />`;
+    })}`;
   }
 }
