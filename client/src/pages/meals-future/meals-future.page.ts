911812195050187bea601699d30efaf65c1ac461
Felix Schulze Sindern <felixs@fh-muenster.de>
2020-12-17 20:55:53 +0100

    Properly render meals.#

--- client/src/pages/meals-future/meals-future.page.ts
+++ client/src/pages/meals-future/meals-future.page.ts
@@ -3,6 +3,11 @@ import { PageMixin } from '../page.mixin';
 import { LanguageStrings } from '../../models/language-strings';
 import { httpService } from '../../services/http.service';
 import { Meal } from '../../../../server/src/models/meal';
+import { AdditivesKeys } from '../../../../scraper/src/models/additives';
+import { AllergenesKeys } from '../../../../scraper/src/models/allergenes';
+import { OtherMealInfoKeys } from '../../../../scraper/src/models/other-meal-info';
+import { LanguageKeys } from '../../i18n/language-keys';
+import { Price } from '../../../../scraper/src/models/price';
 
 const sharedCSS = require('../../shared.scss');
 const componentCSS = require('./meals-future.page.scss');
@@ -40,7 +45,33 @@ class MealsFuturePage extends PageMixin(LitElement) {
       const { title, date, mensa, additives, allergens, otherInfo, price } = meal;
       const entries = Object.entries({ date, mensa, additives, allergens, otherInfo, price });
       return html` <h2>${title}</h2>
-        ${entries.map(([key, value]) => html`<span><b>${key}</b>${value}</span><br />`)} <br />`;
+        ${entries.map(([key, value]) => html`<span><b>${key}:</b> ${this.renderValue(value)}</span><br />`)} <br />`;
     })}`;
+  }
+
+  protected renderValue(item: string | AdditivesKeys[] | AllergenesKeys[] | OtherMealInfoKeys[] | Price): string {
+    if (typeof item === 'string') {
+      return item;
+    } else if (Array.isArray(item)) {
+      return this.renderFoodStr(<string[]>item);
+    } else if (typeof item === 'object') {
+      return this.renderPrice(item);
+    }
+    return '';
+  }
+  protected renderFoodStr(strs: string[]): string {
+    if (strs.length === 0) return '';
+
+    const res = strs.map(i => {
+      const key = <LanguageKeys>String(i);
+      return this.i18n[key];
+    });
+
+    return res.reduce((acc, curr) => acc + ', ' + curr);
+  }
+
+  protected renderPrice(price: Price): string {
+    const { student, employee, guest } = price;
+    return `${student} € - ${employee} € - ${guest} €`;
   }
 }
