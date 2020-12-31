643b5bb17b843702320f472a160d320971dc3960
Felix Schulze Sindern <felixs@fh-muenster.de>
2020-12-31 14:28:40 +0100

    Revert changes

--- client/service-worker.js
+++ client/service-worker.js
@@ -4,11 +4,12 @@ const VERSION = '1.0.0';
 precacheAndRoute(self.__WB_MANIFEST);
 console.log('⚙️ Service Worker attached! ');
 
-self.addEventListener('activate', evt => {
-  evt.waitUntil(
-    caches.keys().then(keys => {
-      console.log(keys);
-      return Promise.all(keys.filter(key => key !== VERSION).map(key => caches.delete(key)));
-    })
-  );
-});
+// self.addEventListener('activate', evt => {
+//   console.log('In activate sw callback');
+//   evt.waitUntil(
+//     caches.keys().then(keys => {
+//       console.log(keys);
+//       return Promise.all(keys.filter(key => key !== VERSION).map(key => caches.delete(key)));
+//     })
+//   );
+// });
