7d46f12d87f45f134e019025240147ea5548eaf8
Felix Schulze Sindern <felixs@fh-muenster.de>
2020-12-31 14:04:48 +0100

    Try to fix font loading by removing publicpath.

--- client/service-worker.js
+++ client/service-worker.js
@@ -1,22 +1,14 @@
-import { mainModule } from 'process';
 import { precacheAndRoute } from 'workbox-precaching/precacheAndRoute';
-// import { storeService } from './src/services/store.service';
-
-// self.addEventListener('install', async () => {
-//   const newVersion = '1';
-//   const oldVersion = await storeService.get('_sw_version');
-//   console.log(`new Version: ${newVersion} old Version: ${oldVersion}`);
-//   if (oldVersion === null) await storeService.set('_sw_version', newVersion);
-//   if (Number(newVersion) !== Number(oldVersion)) {
-//     console.log('New SW Version found - clearing network cache.');
-//     caches.keys().then(cacheNames => {
-//       cacheNames.forEach(cacheName => {
-//         caches.delete(cacheName);
-//       });
-//     });
-//     await storeService.set('_sw_version', newVersion);
-//   }
-// });
+const VERSION = '1.0.0';
 
 precacheAndRoute(self.__WB_MANIFEST);
 console.log('⚙️ Service Worker attached! ');
+
+self.addEventListener('activate', evt => {
+  evt.waitUntil(
+    caches.keys().then(keys => {
+      console.log(keys);
+      return Promise.all(keys.filter(key => key !== VERSION).map(key => caches.delete(key)));
+    })
+  );
+});
--- client/webpack.prod.js
+++ client/webpack.prod.js
@@ -29,7 +29,7 @@ module.exports = {
       },
       {
         test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
-        use: [{ loader: 'file-loader', options: { outputPath: '/fonts/', publicPath: '/mensa-app/fonts' } }]
+        use: [{ loader: 'file-loader', options: { outputPath: '/fonts/' } }]
       }
     ]
   },
