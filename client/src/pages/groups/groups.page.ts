fc0a813f8da1a50ff26eb9b4351748fcf0dcc762
Niclas Schnickmann <niclas.schnickmann@nextstep-services.de>
2021-01-04 18:06:31 +0100

    Fixed group service loop

--- client/src/pages/groups/groups.page.ts
+++ client/src/pages/groups/groups.page.ts
@@ -1,7 +1,10 @@
 import { css, customElement, html, LitElement, property, TemplateResult, unsafeCSS } from 'lit-element';
 import { PageMixin } from '../page.mixin';
 import { LanguageStrings } from '../../models/language-strings';
-
+import { groupService, GroupService } from '../../services/group.service';
+import { Group } from '../../../../server/src/models/group';
+import { repeat } from 'lit-html/directives/repeat';
+import { guard } from 'lit-html/directives/guard';
 const sharedCSS = require('../../shared.scss');
 const componentCSS = require('./groups.page.scss');
 
@@ -17,10 +20,61 @@ class GroupsPage extends PageMixin(LitElement) {
     `
   ];
 
+  @property({ type: Array })
+  protected groups: Group[] = [];
+
+  protected groupService: GroupService = groupService;
+
+  protected async firstUpdated(): Promise<void> {
+    try {
+      groupService.subscribe((groups: Group[]) => {
+        console.log(groups);
+        this.groups = groups;
+      });
+      await groupService.loadGroups(true);
+    } catch ({ message, statusCode }) {
+      if (statusCode === 401) {
+        // routerService.navigate(Routes.SIGN_IN);
+      } else {
+        this.setNotification({ errorMessage: message });
+      }
+    }
+  }
+
   @property({ type: Object, attribute: false })
   protected i18n!: LanguageStrings;
 
   protected render(): TemplateResult {
-    return html` ${this.renderNotification()} Hier könnten Ihre Gruppen stehen! `;
+    return html` ${this.renderNotification()}
+      <ion-content>
+        <ion-list>
+          <ion-list-header> Gruppen </ion-list-header>
+          ${guard(
+            [this.groups],
+            () => html`
+              ${repeat(
+                this.groups,
+                group => group.id,
+                group => html`
+                  <ion-item>
+                    <ion-avatar slot="start">
+                      <img src="" />
+                    </ion-avatar>
+                    <ion-label>
+                      <h2>${group.name}</h2>
+                      <p>Mitglieder: ${group.members.length}</p>
+                    </ion-label>
+                  </ion-item>
+                `
+              )}
+            `
+          )}
+        </ion-list>
+        <ion-fab vertical="bottom" horizontal="end" slot="fixed">
+          <ion-fab-button>
+            <ion-icon name="enter-outline"></ion-icon>
+          </ion-fab-button>
+        </ion-fab>
+      </ion-content>`;
   }
 }
--- client/src/services/group.service.ts
+++ client/src/services/group.service.ts
@@ -4,14 +4,20 @@ import { httpService } from './http.service';
 import { routerService } from './router.service';
 import { storeService } from './store.service';
 
-class GroupService {
+export type GroupsListener = (groups: Group[]) => void;
+export class GroupService {
   protected _groups: Group[] = [];
   protected TASKKEY = 'groups';
+  private listeners: GroupsListener[] = [];
 
-  public async init(): Promise<void> {
+  public subscribe(listener: GroupsListener): void {
+    this.listeners.push(listener);
+  }
+
+  public async loadGroups(scopeMe: boolean): Promise<void> {
     if (navigator.onLine) {
-      const response = await httpService.get('groups' + location.search);
-      const groups = <Group[]>(await response.json()).results;
+      const response = await httpService.get('groups' + (scopeMe ? '?scope=me' : ''));
+      const groups = <Group[]>await response.json();
       await this.setGroups(groups);
     } else {
       let groups = <Group[] | null>await storeService.get(this.TASKKEY);
@@ -20,39 +26,63 @@ class GroupService {
     }
   }
 
+  public async joinByCode(code: string): Promise<void> {
+    if (navigator.onLine) {
+      const body = await (await httpService.get('groups?joincode=' + code)).json();
+      if (body.results.length < 1) {
+        throw new Error('Unkown group');
+      }
+      const groupID = body.results[0].id;
+      await this.addMembership(groupID);
+    } else {
+      return Promise.reject({});
+    }
+  }
   public async createGroup(name: string): Promise<void> {
     if (navigator.onLine) {
-      await httpService.post('groups', { name: name });
+      await httpService.post('groups', { group: { name: name } });
       routerService.navigate(Routes.GROUPS);
     } else {
       return Promise.reject({});
     }
   }
 
-  public async addMembership(groupID: string, userID: string): Promise<void> {
+  public async addMembership(groupID: string): Promise<void> {
     if (navigator.onLine) {
-      await httpService.patch('groups/' + groupID, { members: { operations: 'add', user: userID } });
+      await httpService.post('groups/' + groupID + '/membership', {});
       routerService.navigate(Routes.GROUPS);
     } else {
       return Promise.reject({});
     }
   }
-  public async removeMembership(groupID: string, userID: string): Promise<void> {
+  public async removeMembership(groupID: string, userID?: string): Promise<void> {
     if (navigator.onLine) {
-      await httpService.patch('groups/' + groupID, { members: { operations: 'remove', user: userID } });
+      await httpService.delete('groups/' + groupID + '/membership' + (userID ? '/' + userID : ''));
       routerService.navigate(Routes.GROUPS);
     } else {
       return Promise.reject({});
     }
   }
-
+  public async deleteGroup(groupID: string): Promise<void> {
+    if (navigator.onLine) {
+      await httpService.delete('groups/' + groupID);
+      routerService.navigate(Routes.GROUPS);
+    } else {
+      return Promise.reject({});
+    }
+  }
   protected async setGroups(nGroups: Group[]): Promise<void> {
     this._groups = nGroups;
-    await storeService.set(this.TASKKEY, this.groups);
+    await storeService.set(this.TASKKEY, this._groups);
+    this.notifyListeners();
   }
 
   protected get groups(): Group[] {
     return this._groups;
+  }
+
+  private notifyListeners(): void {
+    this.listeners.forEach(listener => listener(this.groups));
   }
 }
 export const groupService = new GroupService();
