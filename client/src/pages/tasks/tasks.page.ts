542c2285004f27ae0bc21a1a8dbe2934c719a8f2
Tobias Baumeister <tb021023@fh-muenster.de>
2021-01-15 08:05:25 +0100

    Bilder redirect

--- client/src/pages/tasks/tasks.page.ts
+++ client/src/pages/tasks/tasks.page.ts
@@ -117,28 +117,28 @@ class TasksPage extends PageMixin(LitElement) {
         <ion-slides scrollbar="true">
           <ion-slide>
             <h1>
-              <img src="../plan.png" /> <br /><br />
+              <img src="../../images/plan.png" /> <br /><br />
               Was gibt es heute in den Mensen? Hier findest du es heraus! Informationen über die Gerichte aller
               münsteraner Mensen an einem Platz.
             </h1>
           </ion-slide>
           <ion-slide>
             <h1>
-              <img src="zsmessen.png" /><br /><br />
+              <img src="../../images/zsmessen.png" /><br /><br />
               In Gesellschaft ist alles besser! Verabrede dich mit deinen Freunden in einer Mensa.
             </h1>
           </ion-slide>
 
           <ion-slide>
             <h1>
-              <img src="auswahl.png" /><br /><br />
+              <img src="../../images/auswahl.png" /><br /><br />
               Bist du beim Essen wählerisch oder hast einen sensiblen Magen? Dann nutze unsere Filterfunktion und lasse
               dir nur Gerichte anzeigen, die für dich interessant und verträglich sind. <br /><br /><br /><br />
             </h1>
           </ion-slide>
           <ion-slide>
             <h1>
-              <img src="hungrig.png" /><br /><br />
+              <img src="../../images/hungrig.png" /><br /><br />
               Bereits Hunger? Dann lege hier los! <br />
               <ion-button
                 fill="clear"
