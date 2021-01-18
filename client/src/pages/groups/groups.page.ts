ed25e20e7b786745713bcda7d83ab7eaa324e2e0
Felix Schulze Sindern <felixs@fh-muenster.de>
2021-01-18 10:58:33 +0100

    Implement first version of hintTemplate in groups page

--- client/src/pages/groups/groups.page.ts
+++ client/src/pages/groups/groups.page.ts
@@ -8,21 +8,10 @@ import { guard } from 'lit-html/directives/guard';
 import { modalController } from '@ionic/core';
 import { routerService } from '../../services/router.service';
 import { Routes } from '../../routes';
-const sharedCSS = require('../../shared.scss');
-const componentCSS = require('./groups.page.scss');
 
 @customElement('app-groups')
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 class GroupsPage extends PageMixin(LitElement) {
-  static styles = [
-    css`
-      ${unsafeCSS(sharedCSS)}
-    `,
-    css`
-      ${unsafeCSS(componentCSS)}
-    `
-  ];
-
   @property({ type: Array })
   protected groups: Group[] = [];
 
@@ -64,16 +53,11 @@ class GroupsPage extends PageMixin(LitElement) {
         <ion-toolbar>
           <ion-title>${this.i18n.GROUPS}</ion-title>
           <ion-buttons slot="primary">
+            <ion-button @click=${(): void => routerService.navigate(Routes.GROUPS_CREATE)}>
+              <ion-icon name="add"></ion-icon>
+            </ion-button>
             <ion-button @click=${() => routerService.navigate(Routes.SETTINGS)}>
               <ion-icon slot="icon-only" name="settings-outline"></ion-icon>
-              <!-- <ion-icon name="person-circle"></ion-icon> -->
-              <!-- TODO: Make Google style avatar work -->
-              <!-- <ion-avatar style="border-radius: 0px" slot="end">
-                <img
-                  style="width: 60px; height:60px"
-                  src="https://www.scherenzauber.de/wp-content/uploads/Google-Avatar.png"
-                />
-              </ion-avatar> -->
             </ion-button>
           </ion-buttons>
         </ion-toolbar>
@@ -82,15 +66,12 @@ class GroupsPage extends PageMixin(LitElement) {
         <ion-header collapse="condense">
           <ion-toolbar>
             <ion-title size="large">${this.i18n.GROUPS}</ion-title>
-            <ion-buttons slot="primary">
-              <ion-button @click=${(): void => routerService.navigate(Routes.GROUPS_CREATE)}
-                ><ion-icon name="add"></ion-icon
-              ></ion-button>
-            </ion-buttons>
           </ion-toolbar>
         </ion-header>
+
+        ${this.groups.length === 0 || !this.userInfo ? this.hintTemplate : ''}
+
         <ion-list lines="inset">
-          <ion-list-header> Gruppen </ion-list-header>
           ${guard(
             [this.groups],
             () => html`
@@ -127,5 +108,56 @@ class GroupsPage extends PageMixin(LitElement) {
           </ion-fab-button>
         </ion-fab>
       </ion-content>`;
+  }
+
+  protected get hintTemplate(): TemplateResult {
+    return html`
+      <ion-card class="card-no-margin-when-small">
+        <ion-card-header>
+          ${this.userInfo
+            ? html`<img
+                style="margin-bottom:16px; max-height:300px; display: block;margin-left: auto;margin-right: auto;"
+                src="./svg/zsmessen.svg"
+              />`
+            : ''}
+          <ion-card-title>Plane Mensabesuche mit deinen Freunden!</ion-card-title>
+        </ion-card-header>
+        <ion-card-content style="font-size: 16px;">
+          Klicke auf das
+          <ion-icon color="primary" name="add"></ion-icon> Symbol um eine Gruppe zu erstellen oder klicke auf das
+          <ion-icon color="primary" name="enter-outline"></ion-icon> Symbol um einer Gruppe beizutreten.
+          <!-- ${!this.userInfo
+            ? html`
+                <br />
+                <ion-note style="font-size: 16px;" color="warning">
+                  Du muss dich anmelden um Gruppen erstellen und beitreten zu können!
+                </ion-note>
+              `
+            : ''} -->
+        </ion-card-content>
+        ${!this.userInfo
+          ? html`
+              <!-- <ion-item-divider mode="ios">
+                <ion-label color="warning">
+                  Du muss dich anmelden um Gruppen erstellen und beitreten zu können!
+                </ion-label>
+              </ion-item-divider> -->
+              <ion-card style="--background: rgba(var(--ion-color-warning-rgb), .5); margin-top:0px; margin-bottom:0px">
+                <ion-card-content style="font-size: 16px;">
+                  Du muss dich anmelden um Gruppen erstellen und beitreten zu können!</ion-card-content
+                >
+              </ion-card>
+              <ion-item href=${Routes.SIGN_IN} .detail="${false}" style="--background: var(--ion-card-background)">
+                <ion-label>Jetzt anmelden</ion-label>
+                <ion-button fill="outline" slot="end">Anmelden</ion-button>
+              </ion-item>
+              <ion-item href=${Routes.SIGN_UP} .detail="${false}" style="--background: var(--ion-card-background)">
+                <ion-label>Jetzt registrieren</ion-label>
+                <ion-button fill="outline" slot="end">Registrieren</ion-button>
+              </ion-item>
+            `
+          : ''}
+      </ion-card>
+    `;
   }
 }
