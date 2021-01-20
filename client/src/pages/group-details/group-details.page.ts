5cf5efafd84d60259d1052c1c531b213c054e5ed
Felix Schulze Sindern <felixs@fh-muenster.de>
2021-01-20 15:35:26 +0100

    Copy card to group detail page

--- client/src/pages/group-details/group-details.page.ts
+++ client/src/pages/group-details/group-details.page.ts
@@ -1,124 +1,131 @@
-/* eslint-disable @typescript-eslint/member-ordering */
-import { css, customElement, html, LitElement, property, TemplateResult, unsafeCSS } from 'lit-element';
+import { customElement, html, internalProperty, LitElement, property, query, TemplateResult } from 'lit-element';
 import { PageMixin } from '../page.mixin';
 import { LanguageStrings } from '../../models/language-strings';
 import { groupService, GroupService } from '../../services/group.service';
 import { Group } from '../../../../server/src/models/group';
 import { User } from '../../../../server/src/models/user';
 import { routerService } from '../../services/router.service';
-import { repeat } from 'lit-html/directives/repeat';
-import { guard } from 'lit-html/directives/guard';
-const sharedCSS = require('../../shared.scss');
-const componentCSS = require('./group-details.page.scss');
 
 @customElement('app-group-details')
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 class CreateGroupPage extends PageMixin(LitElement) {
-  static styles = [
-    css`
-      ${unsafeCSS(sharedCSS)}
-    `,
-    css`
-      ${unsafeCSS(componentCSS)}
-    `
-  ];
-
   protected groupService: GroupService = groupService;
   protected groupID = routerService.getQueryParameter('id');
-  protected group: Group | undefined;
+  @internalProperty()
+  protected group?: Group;
+  @internalProperty()
   protected members: User[] | undefined;
   @property({ type: Object, attribute: false })
   protected i18n!: LanguageStrings;
+
+  protected scrollPos: any;
+  protected isDragging: boolean = false;
+
+  @query('.horizontal-scroll-inner-container')
+  protected innerScrollContainer!: HTMLDivElement;
+
   protected async firstUpdated(): Promise<void> {
     this.groupService.getGroup(this.groupID).then(res => {
       this.group = res;
-      this.requestUpdate();
     });
     this.groupService.getGroupMembers(this.groupID).then(res => {
       this.members = res;
-      this.requestUpdate();
     });
   }
+
   protected formatDate = (unixMillis: number): string => {
     const date = new Date(unixMillis);
     return date.toLocaleString('de-DE');
   };
+
   protected render(): TemplateResult {
     return html`
-      <ion-header>
+      <ion-header style="background-color: var(--ion-background-color);">
         <ion-toolbar>
           <ion-buttons slot="start">
             <ion-back-button
-              @click=${async (): Promise<void> => {
+              @click=${(): void => {
                 history.back();
               }}
               .text="${this.mode === 'ios' ? this.i18n.BACK : null}"
             ></ion-back-button>
           </ion-buttons>
-          <ion-title>${this.i18n.GROUP_DETAILS}</ion-title>
         </ion-toolbar>
       </ion-header>
       <ion-content class="ion-padding">
-        <ion-header collapse="condense">
-          <ion-toolbar>
-            <ion-title size="large">${this.i18n.GROUP_DETAILS} - ${this.group?.name}</ion-title>
-          </ion-toolbar>
-        </ion-header>
-        <ion-list class="ion-padding">
-          <ion-list-header>Termine</ion-list-header>
-          <ion-item><ion-label>TODO</ion-label></ion-item>
-        </ion-list>
-
-        <ion-list class="ion-padding">
-          <ion-list-header>Details</ion-list-header>
-          <ion-item>
-            <ion-label>Name</ion-label>
-            <ion-label>${this.group?.name}</ion-label>
-          </ion-item>
-          <ion-item>
-            <ion-label>${this.i18n.JOINCODE}</ion-label>
-            <ion-label
-              >${this.group?.joinCode} <ion-button><ion-icon name="share-social-outline"></ion-icon></ion-button
-            ></ion-label>
-          </ion-item>
-          <ion-item>
-            <ion-label>Erstellt am</ion-label>
-            <ion-label>${this.group && this.formatDate(this.group!.createdAt)}</ion-label>
-          </ion-item>
-        </ion-list>
-        <ion-list class="ion-padding">
-          <ion-list-header>Mitglieder</ion-list-header>
-          ${this.members &&
-          guard(
-            [this.members],
-            () => html`
-              ${repeat(
-                this.members!,
-                member => member.id,
-                member => html`
-                  <ion-item>
-                    <ion-label>${member.name}</ion-label>
-                    <ion-button
-                      @click=${(): void => {
-                        this.groupService.removeMembership(this.groupID, member.id);
-                      }}
-                      ><ion-icon name="trash-outline"></ion-icon
-                    ></ion-button>
-                  </ion-item>
-                `
-              )}
-            `
-          )}
-        </ion-list>
-        <div class="ion-padding" style="display: flex; justify-content:center">
-          <ion-button
-            @click=${(): void => {
-              this.groupService.removeMembership(this.groupID);
-            }}
-            >Gruppe verlassen</ion-button
-          >
-        </div>
+        ${// ### helpful stuff ###
+        // this.i18n.JOINCODE
+        // this.group && this.formatDate(this.group!.createdAt)
+        // this.members
+        // @click=${(): void => {
+        //   this.groupService.removeMembership(this.groupID);
+        // }}
+        ''}
+        <ion-card class="card-no-margin-when-small">
+          <div class="bg-image-wrapper">
+            <img class="bg-image" src=${this.group?.image?.url || '/images/default_group_background.png'} />
+          </div>
+          <ion-avatar class="group-list-avatar" style="background-color:white">
+            ${this.group?.image?.url
+              ? html`<img src=${this.group.image?.url || ''} />`
+              : html`<ion-icon
+                  style="width:100%;height:100%;transform: scale(1.23);"
+                  color="primary"
+                  name="people-circle-outline"
+                ></ion-icon>`}
+          </ion-avatar>
+          <ion-card-header style="padding-top:0px">
+            ${this.buttonsTemplate}
+            <ion-card-subtitle>
+              ${this.group?.members.length}
+              ${this.group?.members && this.group?.members.length > 1 ? this.i18n.MEMBERS : this.i18n.MEMBER}
+            </ion-card-subtitle>
+            <ion-card-title style="display:flex"> ${this.group?.name} </ion-card-title>
+          </ion-card-header>
+        </ion-card>
       </ion-content>
+    `;
+  }
+
+  protected onMouseDown = (e: any) => {
+    this.isDragging = true;
+    this.scrollPos = {
+      // The current scroll
+      left: this.innerScrollContainer.scrollLeft,
+      // Get the current mouse position
+      x: e.clientX
+    };
+    this.innerScrollContainer.style.userSelect = 'none';
+    document.addEventListener('mousemove', this.onMouseMove);
+    document.addEventListener('mouseup', this.onMouseUp);
+  };
+
+  protected onMouseMove = (e: any) => {
+    // How far the mouse has been moved
+    const dx = e.clientX - this.scrollPos.x;
+    // Scroll the element
+    this.innerScrollContainer.scrollLeft = this.scrollPos.left - dx;
+  };
+
+  protected onMouseUp = (e: any) => {
+    this.innerScrollContainer.style.removeProperty('user-select');
+    this.isDragging = false;
+    document.removeEventListener('mousemove', this.onMouseMove);
+    document.removeEventListener('mouseup', this.onMouseUp);
+  };
+
+  protected get buttonsTemplate(): TemplateResult {
+    return html`
+      <ion-buttons style="position:absolute; right:0px; top:0px; z-index:999; padding:4px">
+        <ion-button
+          @click=${(e: any) => {
+            e.stopPropagation();
+            console.log('TODO: Implement Share API to Invite someone to a group');
+          }}
+        >
+          <ion-icon slot="icon-only" color="primary" name="share-social"></ion-icon>
+        </ion-button>
+      </ion-buttons>
     `;
   }
 }
