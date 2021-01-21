386b2bece0910c6b978c72719372b646031d8158
Felix Schulze Sindern <felixs@fh-muenster.de>
2021-01-21 14:24:06 +0100

    improve styles of large mensa visit component

--- client/src/widgets/group-date/group-date.widget.ts
+++ client/src/widgets/group-date/group-date.widget.ts
@@ -95,9 +95,11 @@ export class GroupDateWidget extends LitElement {
               `
             )}
           </app-horizontal-scroller>
-          <div><ion-icon style="color: var(--ion-text-color)" name="location-outline"></ion-icon> Mensa am Ring</div>
-          <div><ion-icon style="color: var(--ion-text-color)" name="calendar-outline"></ion-icon> 01.02.21</div>
-          <div><ion-icon style="color: var(--ion-text-color)" name="time-outline"></ion-icon> 13:30</div>
+          <div style="color: var(--ion-text-color)">
+            <div><ion-icon name="location-outline"></ion-icon> Mensa am Ring</div>
+            <div><ion-icon name="calendar-outline"></ion-icon> 01.02.21</div>
+            <div><ion-icon name="time-outline"></ion-icon> 13:30</div>
+          </div>
         </ion-card-content>
       </ion-card>
     `;
