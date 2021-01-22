f88b788af8cba6a2bd0b390d1021e89b53559a96
Niclas Schnickmann <niclas.schnickmann@nextstep-services.de>
2021-01-22 10:48:41 +0100

    Implemented i18n tempates

--- client/src/services/i18n.service.ts
+++ client/src/services/i18n.service.ts
@@ -34,6 +34,9 @@ class I18nService {
   public getStrings(): LanguageStrings {
     return this.strings;
   }
+  public complexi18n(languageString: string, params: { [key: string]: string }): string {
+    return this.renderSentenceWithParams(languageString, params);
+  }
 
   public subscribe(listener: i18nListener): void {
     this.listeners.push(listener);
@@ -41,6 +44,21 @@ class I18nService {
 
   private notifyListeners(i18n: LanguageStrings): void {
     this.listeners.forEach(listener => listener(i18n));
+  }
+  private renderSentenceWithParams(sentence: string, params: { [key: string]: string }): string {
+    const paramKeys = Object.keys(params);
+    let targetStr = sentence;
+
+    if (paramKeys.length > 0) {
+      for (const p in paramKeys) {
+        const value = params['test'];
+        if (typeof value === 'string') {
+          targetStr = targetStr.replace(new RegExp('\\{' + p + '\\}', 'gi'), value);
+        }
+      }
+    }
+
+    return targetStr;
   }
 }
 
