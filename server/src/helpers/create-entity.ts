2faee54220f84c37d3d557d40a385ceec4680fec
Felix Schulze Sindern <felixs@fh-muenster.de>
2021-01-22 15:22:45 +0100

    Try new offline strategy

---
+++ server/src/helpers/create-entity.ts
@@ -0,0 +1,9 @@
+import { v4 as uuidv4 } from 'uuid';
+import { Entity } from '../../../server/src/models/entity';
+
+export function createEntity(): Entity {
+  return {
+    id: uuidv4(),
+    createdAt: new Date().getTime()
+  };
+}
