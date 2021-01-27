9cd1b4bca4c20a4fd138d5c30f686df955fc8332
Tobias Baumeister <tb021023@fh-muenster.de>
2021-01-27 12:15:39 +0100

    Login Profil Add

--- client/src/widgets/profile/profile.widget.ts
+++ client/src/widgets/profile/profile.widget.ts
@@ -6,6 +6,8 @@ import { i18nService } from '../../services/i18n.service';
 import { share, ShareParameter } from '../../helpers/share-api';
 import { Routes } from '../../routes';
 import { createShareModal } from '../../helpers/create-share-modal';
+import { userService } from '../../services/user.service';
+import { User } from '../../../../server/src/models/user';
 @customElement('app-profile')
 export class ProfileWidget extends LitElement {
   protected createRenderRoot(): LitElement {
@@ -15,10 +17,18 @@ export class ProfileWidget extends LitElement {
   @property({ type: Object, attribute: false })
   protected i18n!: LanguageStrings;
 
+  @property({ type: Object })
+  protected userInfo?: User;
+
+  @property({ type: Object, attribute: false })
+  protected setNotification!: (e: any) => void;
+
   constructor() {
     super();
     this.i18n = i18nService.getStrings();
     i18nService.subscribe(i18n => (this.i18n = i18n));
+    this.userInfo = userService.userInfo;
+    userService.subscribe(userInfo => (this.userInfo = userInfo));
   }
 
   protected render(): TemplateResult {
@@ -29,7 +39,42 @@ export class ProfileWidget extends LitElement {
           <ion-card-title style="display:flex">Name </ion-card-title>
         </ion-card-header>
         <ion-card-content style="display:flex">Test Test </ion-card-content>
+        ${this.userInfo
+          ? html`
+              <ion-item>
+                <ion-label>${this.i18n.SIGN_OUT}</ion-label>
+                <ion-button @click=${this.logOut}>${this.i18n.SIGN_OUT}</ion-button>
+              </ion-item>
+            `
+          : html`
+              <ion-item
+                class="item-inside-card"
+                href=${Routes.SIGN_IN}
+                .detail="${false}"
+                style="--border-color: var();--background: var(--ion-card-background)"
+              >
+                <ion-label>${this.i18n.SIGN_IN_NOW}</ion-label>
+                <ion-button fill="outline" slot="end">${this.i18n.SIGN_IN}</ion-button>
+              </ion-item>
+              <ion-item
+                lines="none"
+                href=${Routes.SIGN_UP}
+                .detail="${false}"
+                style="--background: var(--ion-card-background)"
+              >
+                <ion-label>${this.i18n.SIGN_UP_NOW}</ion-label>
+                <ion-button fill="outline" slot="end">${this.i18n.SIGN_UP}</ion-button>
+              </ion-item>
+            `}
       </ion-card>
     `;
+  }
+  protected async logOut(): Promise<void> {
+    try {
+      await userService.logOut();
+      this.setNotification({ successMessage: this.i18n.SIGN_OUT_MESSAGE + '!' });
+    } catch ({ message }) {
+      this.setNotification({ errorMessage: message });
+    }
   }
 }
